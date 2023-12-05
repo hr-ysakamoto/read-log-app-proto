c30f63d0388fb4192e8d9cfff9b5718643b189d8
Yuki Sakamoto <106657355+hr-ysakamoto@users.noreply.github.com>
2023-12-05 18:19:05 +0900

    Zodの型定義からopen-apiを吐けるようになった

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,15 +1,91 @@
-import fastify from "fastify";
+import * as fs from "fs";
+import * as path from "path";
 
-const server = fastify();
+import fastify, { FastifyBaseLogger, FastifyInstance } from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import {
+  ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { UserInputSchema, UserOutputSchema } from "@repo/models/types";
+import { Server, IncomingMessage, ServerResponse } from "node:http";
 
-server.get("/ping", async (request, reply) => {
-  return "pong\n";
-});
+export type FastifyZodInstance = FastifyInstance<
+  Server<typeof IncomingMessage, typeof ServerResponse>,
+  IncomingMessage,
+  ServerResponse<IncomingMessage>,
+  FastifyBaseLogger,
+  ZodTypeProvider
+>;
 
-server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err);
+const host = "0.0.0.0" as const;
+const port = 8000 as const;
+
+export const fastifyApp = () =>
+  fastify({
+    logger: true,
+  }).withTypeProvider<ZodTypeProvider>();
+
+const main = async (app: FastifyZodInstance) => {
+  try {
+    app.setValidatorCompiler(validatorCompiler);
+    app.setSerializerCompiler(serializerCompiler);
+    app.register(fastifySwagger, {
+      openapi: {
+        info: {
+          title: "Book Record API",
+          description: "Sample backend service",
+          version: "1.0.0",
+        },
+        servers: [],
+      },
+      transform: jsonSchemaTransform,
+    });
+
+    app.register(fastifySwaggerUI, {
+      routePrefix: "/documentation",
+    });
+
+    app.after(() => {
+      app.withTypeProvider<ZodTypeProvider>().route({
+        method: "GET",
+        url: "/user",
+        schema: {
+          querystring: UserInputSchema,
+          response: {
+            200: UserOutputSchema,
+          },
+        },
+        handler: (req, res) => {
+          console.log({ id: req.query.userId });
+          res.send({ userName: "John Doe" });
+        },
+      });
+    });
+    await app.ready();
+    const swaggerJson = app
+      .withTypeProvider<ZodTypeProvider>()
+      .swagger({ yaml: false });
+
+    const yaml = app.swagger({ yaml: true });
+    const openApiDir = path.resolve(__dirname, "../../packages/models/api");
+    const swaggerYamlPath = `${openApiDir}/swagger.yaml` as const;
+    if (!fs.existsSync(openApiDir)) {
+      fs.mkdirSync(openApiDir, { recursive: true });
+    }
+    fs.writeFileSync(swaggerYamlPath, yaml);
+    console.log(`generated routed >>> ${swaggerYamlPath}`);
+
+    await app.listen({ port, host });
+    console.log(`Server listining on port ${port}`);
+  } catch (error) {
+    console.error(error);
     process.exit(1);
   }
-  console.log(`Server listening at ${address}`);
-});
+};
+
+export const app = fastifyApp();
+main(app);
---
+++ packages/models/lib/zod.ts
@@ -0,0 +1,5 @@
+import { z } from "zod";
+
+export type ToZod<T extends Record<string, any>> = {
+  [K in keyof T]-?: z.ZodType<T[K]>;
+};
